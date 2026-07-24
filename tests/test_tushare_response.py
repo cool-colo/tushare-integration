6863534e67a5de49e94216561c7a5bdaaa972417
fanglicheng <fanglicheng@baoyun.com>
2026-07-24 15:05:35 +0800

    update

--- tests/test_tushare_response.py
+++ tests/test_tushare_response.py
@@ -23,6 +23,7 @@ from tushare_integration.dws import (
 from tushare_integration.manager import CrawlManager
 from tushare_integration.spiders.index.quotes import IndexWeightSpider
 from tushare_integration.spiders.stock.basic import STSpider
+from tushare_integration.spiders.stock.financial import FinaMainBZSpider
 from tushare_integration.spiders.stock.special import CyqChipsSpider
 
 
@@ -695,6 +696,11 @@ class TushareResponseTest(unittest.TestCase):
         self.assertEqual(spider.schema["primary_key"], ["ts_code", "pub_date", "imp_date", "st_type"])
         self.assertIn("st_type", [column["name"] for column in spider.schema["columns"]])
         self.assertNotIn("st_tpye", [column["name"] for column in spider.schema["columns"]])
+
+    def test_fina_mainbz_primary_key_preserves_business_items(self):
+        spider = FinaMainBZSpider()
+
+        self.assertEqual(spider.schema["primary_key"], ["ts_code", "end_date", "bz_item", "update_flag"])
 
     def test_index_weight_start_requests_uses_documented_monthly_index_code_params(self):
         spider = IndexWeightSpider()
